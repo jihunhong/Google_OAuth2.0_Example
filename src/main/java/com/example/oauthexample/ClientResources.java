ca1ef3d1cdfc14bc26669282cbc75958149ab5f2
jihunhong <redgee49@gmail.com>
2019-06-16 03:15:59 +0900

    스프링 부트 2.0 OAuth2 사용을 위한 코드 분리 및 의존성 설정

--- src/main/java/com/example/oauthexample/ClientResources.java
+++ src/main/java/com/example/oauthexample/ClientResources.java
@@ -1,23 +1,23 @@
-package com.example.oauthexample;
+// package com.example.oauthexample;
 
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+// import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+// import org.springframework.boot.context.properties.NestedConfigurationProperty;
+// import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 
-public class ClientResources {
+// public class ClientResources {
 
-    @NestedConfigurationProperty
-    private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+//     @NestedConfigurationProperty
+//     private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
 
-    @NestedConfigurationProperty
-    private ResourceServerProperties resource = new ResourceServerProperties();
+//     @NestedConfigurationProperty
+//     private ResourceServerProperties resource = new ResourceServerProperties();
 
-    public AuthorizationCodeResourceDetails getClient(){
-        return client;
-    }
+//     public AuthorizationCodeResourceDetails getClient(){
+//         return client;
+//     }
 
-    public ResourceServerProperties getResource(){
-        return resource;
-    }
+//     public ResourceServerProperties getResource(){
+//         return resource;
+//     }
 
-}
+// }
